e6b617b4099554535a50e0ea357328b3fa8acbf0
Oleg Balunenko <obalunenko@partners.luno.com>
2018-12-11 03:28:05 +0300

    pkg: create file with constants

---
+++ pkg/constants.go
@@ -0,0 +1,16 @@
+package pkg
+
+const ( // env variables
+	JWTSecret     = "jwt_secret"
+	RefreshSecret = "refresh_secret"
+)
+
+const ( // Cookie token keys names
+	AuthToken    = "authToken"
+	RefreshToken = "refreshToken"
+)
+
+const (
+	captionThisBackend  = "captionthis-backend"
+	captionThisFrontend = "captionthis-frontend"
+)
